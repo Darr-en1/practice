d0ab3ed661c9a52dee541198923937bf30700ed5
darr_en1 <darr_en1@126.com>
2019-04-26 16:11:22 +0800

    最大数

--- AlgorithmProblem/最大数.py
+++ AlgorithmProblem/最大数.py
@@ -1,1 +1,23 @@
 __author__ = 'Darr_en1'
+
+# 给定一组非负整数，重新排列它们的顺序使之组成一个最大的整数。
+import re
+
+class Solution:
+    def largestNumber(self, nums):
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(0, n - 1 - i):
+                if int(str(nums[j]) + str(nums[j+1])) < int(str(nums[j+1]) + str(nums[j])):
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+        return re.sub("^0+","0","".join([str(val) for val in nums]))
+
+
+
+if __name__ == '__main__':
+    res = Solution().largestNumber([3,30,34,5,9])
+    re2 = Solution().largestNumber([0,0])
+    print(res)
+    print(re2)
+

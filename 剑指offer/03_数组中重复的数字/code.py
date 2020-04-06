3b6b4d5b495510bcb7a7f6e0fa0051bb2f6e7ddd
zt <darr_en1@126.com>
2020-04-06 17:25:21 +0800

    数组中重复数字

--- 剑指offer/03_数组中重复的数字/code.py
+++ 剑指offer/03_数组中重复的数字/code.py
@@ -1,10 +1,31 @@
-def duplicate(numbers):
-    if not numbers:
+import random
+
+
+def duplicate(nums):
+    if not nums:
         return None
 
-    for index, value in enumerate(numbers):
+    for index, value in enumerate(nums):
         while index != value:
-            if value == numbers[value]:
+            if nums[index] == nums[value]:
                 return value
-            value, numbers[value] = numbers[value], value
+            nums[index], nums[value] = nums[value], nums[index]
+            value = nums[index]  # 获取交换后的value
     return None
+
+
+def random_list(length, repeat=False):
+    if repeat:
+        return [random.randint(0, length - 2) for _ in range(length)]
+    else:
+        res = [i for i in range(length)]
+        random.shuffle(res)
+        return res
+
+
+def test_exist_repeat():
+    assert type(duplicate(random_list(length=10, repeat=True))) == int
+
+
+def test_not_exist_repeat():
+    assert duplicate(random_list(length=10)) is None

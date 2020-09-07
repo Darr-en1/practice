2fed094c61e4454327f493649209ab739ee455b0
zt <darr_en1@126.com>
2020-09-07 19:33:37 +0800

    找出数组中的重复数字

--- 剑指offer/04_二维数组中的查找/code.py
+++ 剑指offer/04_二维数组中的查找/code.py
@@ -1,34 +1,18 @@
-import random
+from typing import List
 
 
-def find(arr,num):
-    if not arr:
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        if matrix:
+            a = 0
+            b = len(matrix[a]) - 1
+            while a < len(matrix) and b >= 0:
+                if matrix[a][b] > target:
+                    b -= 1
+                elif matrix[a][b] < target:
+                    a += 1
+                else:
+                    return True
         return False
 
-    for index, value in enumerate(arr):
-        for idx, val in enumerate(value):
-            if val > num:
 
-            if nums[in dex] == nums[value]:
-                return value
-            nums[index], nums[value] = nums[value], nums[index]
-            value = nums[index]  # 获取交换后的value
-
-    return None
-
-
-def random_list(length, repeat=False):
-    if repeat:
-        return [random.randint(0, length - 2) for _ in range(length)]
-    else:
-        res = [i for i in range(length)]
-        random.shuffle(res)
-        return res
-
-
-def test_exist_repeat():
-    assert type(duplicate(random_list(length=10, repeat=True))) == int
-
-
-def test_not_exist_repeat():
-    assert duplicate(random_list(length=10)) is None

cae1637a36986547964822829550172a2d0adbf7
zt <darr_en1@126.com>
2020-09-07 19:34:00 +0800

    找出数组中的重复数字

--- 剑指offer/03_数组中重复的数字/code.py
+++ 剑指offer/03_数组中重复的数字/code.py
@@ -2,16 +2,15 @@ import random
 
 
 def duplicate(nums):
-    if not nums:
-        return None
-
-    for index, value in enumerate(nums):
-        while index != value:
-            if nums[index] == nums[value]:
-                return value
-            nums[index], nums[value] = nums[value], nums[index]
-            value = nums[index]  # 获取交换后的value
-    return None
+    i = 0
+    while i < len(nums):
+        val = nums[i]
+        if i == val:
+            i += 1
+        elif nums[i] == nums[val]:
+            return val
+        else:
+            nums[i], nums[val] = nums[val], nums[i]
 
 
 def random_list(length, repeat=False):
@@ -21,11 +20,3 @@ def random_list(length, repeat=False):
         res = [i for i in range(length)]
         random.shuffle(res)
         return res
-
-
-def test_exist_repeat():
-    assert type(duplicate(random_list(length=10, repeat=True))) == int
-
-
-def test_not_exist_repeat():
-    assert duplicate(random_list(length=10)) is None

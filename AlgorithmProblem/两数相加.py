613512a273beb8072582150b7be8bdd08b27f4f0
zt <darr_en1@126.com>
2020-09-07 19:34:18 +0800

    两数相加

--- AlgorithmProblem/两数相加.py
+++ AlgorithmProblem/两数相加.py
@@ -1,47 +1,36 @@
-"""
-给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
-
-如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
-
-您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
-
-示例：
-
-输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
-输出：7 -> 0 -> 8
-原因：342 + 465 = 807
-"""
-
-
-# Definition for singly-linked list.
-class ListNode(object):
+class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
-class Solution(object):
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        pre = curr = ListNode(0)
+        more = 0
+        while l1 or l2 or more:
+            num = (l1.val if l1 else 0) + (l2.val if l2 else 0) + more
+            curr.next = ListNode(num % 10)
+            curr = curr.next
+            more = num // 10
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
-        re = ListNode(0)
-        r = re
-        carry = 0
-        while l1 or l2:
-            x = l1.val if l1 else 0
-            y = l2.val if l2 else 0
-            s = carry + x + y
-            carry = s // 10
-            r.next = ListNode(s % 10)
-            r = r.next
-            if l1 is not None:
-                l1 = l1.next
-            if l2 is not None:
-                l2 = l2.next
-        if carry > 0:
-            r.next = ListNode(1)
-        return re.next
+        return pre.next
+
+
+def arrToList(arr):
+    pre = curr = ListNode(0)
+    for val in arr:
+        curr.next = ListNode(val)
+        curr = curr.next
+    return pre.next
+
+
+arr1 = [2, 4, 3]
+arr2 = [5, 6, 4]
+
+l1 = arrToList(arr1)
+l2 = arrToList(arr2)
+print(Solution().addTwoNumbers(l1, l2))
+pass

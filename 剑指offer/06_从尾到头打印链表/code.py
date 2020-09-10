1245425606387b6d5a84832ea104203ef2f8bd60
zt <darr_en1@126.com>
2020-09-10 15:12:11 +0800

    从尾到头打印链表

---
+++ 剑指offer/06_从尾到头打印链表/code.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        return self.reversePrint(head.next) + [head.val] if head else []

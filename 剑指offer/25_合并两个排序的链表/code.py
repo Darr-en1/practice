d9050341c109649d508dd81ea4800d077195bb66
zt <darr_en1@126.com>
2020-09-14 19:53:29 +0800

    合并两个排序的链表

---
+++ 剑指offer/25_合并两个排序的链表/code.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        pre = curr = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next, l1 = l1, l1.next
+            else:
+                curr.next, l2 = l2, l2.next
+            curr = curr.next
+
+        curr.next = l1 if l1 else l2
+        return pre.next

31715ccf793508a77a4cd5765669f6a89add132b
zt <darr_en1@126.com>
2020-09-18 16:48:16 +0800

    字符串的排列

--- 剑指offer/38_字符串的排列/code.py
+++ 剑指offer/38_字符串的排列/code.py
@@ -3,21 +3,24 @@ from typing import List
 
 class Solution:
     def permutation(self, s: str) -> List[str]:
-        c, res = list(s), []
+        a, res = list(s), []
 
-        def dfs(x):
-            if x == len(c) - 1:
-                res.append(''.join(c))  # 添加排列方案
+        def inner(x):
+            if x == len(a) - 1:
+                res.append(''.join(a))
                 return
-            dic = set()
-            for i in range(x, len(c)):
-                if c[i] in dic: continue  # 重复，因此剪枝
-                dic.add(c[i])
-                c[i], c[x] = c[x], c[i]  # 交换，将 c[i] 固定在第 x 位
-                dfs(x + 1)  # 开启固定第 x + 1 位字符
-                c[i], c[x] = c[x], c[i]  # 恢复交换
 
-        dfs(0)
+            exist = set()
+            for i in range(x, len(s)):
+                if a[i] in exist:
+                    continue
+
+                exist.add(a[i])
+                a[x], a[i] = a[i], a[x]
+                inner(x + 1)
+                a[x], a[i] = a[i], a[x]
+
+        inner(0)
         return res
 
 

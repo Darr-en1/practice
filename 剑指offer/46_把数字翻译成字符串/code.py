26fafb7579cf7cb03a99003cbb2ad4c0b3161999
zt <darr_en1@126.com>
2020-09-19 16:06:44 +0800

    把数字翻译成字符串

---
+++ 剑指offer/46_把数字翻译成字符串/code.py
@@ -0,0 +1,20 @@
+class Solution:
+    def translateNum(self, num: int) -> int:
+        re_str = str(num)
+        a = 0
+
+        def inner(idx):
+            if idx >= len(re_str) - 1:
+                nonlocal a
+                a += 1
+                return
+            inner(idx + 1)
+            print(re_str[idx:idx + 2])
+            if "10" <= re_str[idx:idx + 2] < "26":
+                inner(idx + 2)
+
+        inner(0)
+        return a
+
+
+print(Solution().translateNum(1068385902))

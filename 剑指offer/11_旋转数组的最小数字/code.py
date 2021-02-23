c8bd6683e6ec1e393d79f1e7bcbf9403b9d4fcc0
darr_en1 <714970206@qq.com>
2021-02-23 16:27:44 +0800

    旋转数组的最小数字

--- 剑指offer/11_旋转数组的最小数字/code.py
+++ 剑指offer/11_旋转数组的最小数字/code.py
@@ -4,19 +4,6 @@ from typing import List
 class Solution:
     def minArray(self, numbers: List[int]) -> int:
         """:raise
-        常规
-        """
-        i, j = 0, 1
-        while i < len(numbers) - 1:
-            if numbers[i] <= numbers[j]:
-                i += 1
-                j += 1
-            else:
-                return numbers[j]
-        return numbers[0]
-
-    def minArray2(self, numbers: List[int]) -> int:
-        """:raise
         二分查找
         """
         i, j = 0, len(numbers) - 1
